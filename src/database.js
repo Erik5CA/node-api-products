8b530af3b00c91fd195226f499e119bead2489b3
Erik5CA <erik.castillo.anzures@gmail.com>
2024-12-12 17:36:02 -0600

    change connection mongodb url

--- src/database.js
+++ src/database.js
@@ -1,8 +1,13 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const MONGO_URL = process.env.MONGO_URL || "mongodb://localhost/productsdb";
 
 export const conncectToDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost/productsdb");
+    await mongoose.connect(MONGO_URL);
     console.log("Connected to DB");
   } catch (error) {
     console.log(error);
