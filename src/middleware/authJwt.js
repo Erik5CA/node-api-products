252eaa2dc807245c13b6133e105f66dc934b69bd
Erik5CA <erik.castillo.anzures@gmail.com>
2024-12-13 15:38:10 -0600

    add new middleware to verify is user is moderator or admin

--- src/middleware/authJwt.js
+++ src/middleware/authJwt.js
@@ -1,11 +1,11 @@
 import jwt from "jsonwebtoken";
 import config from "../config.js";
 import User from "../models/user.model.js";
+import Role from "../models/role.model.js";
 
 export const verifyToken = async (req, res, next) => {
   try {
     const token = req.headers["x-access-token"];
-    console.log({ token });
     if (!token) return res.status(403).json({ message: "No token provided" });
 
     const decoded = jwt.verify(token, config.SECRET);
@@ -14,10 +14,32 @@ export const verifyToken = async (req, res, next) => {
     const user = await User.findById(id, { password: 0 });
     if (!user) return res.status(404).json({ message: "User not found" });
 
-    console.log(user);
     next();
   } catch (error) {
-    console.log(error);
     res.status(401).json({ message: "Unauthorized" });
   }
+};
+
+export const isModerator = async (req, res, next) => {
+  const user = await User.findById(req.userId);
+  const roles = await Role.find({ _id: { $in: user.roles } });
+  for (let i = 0; i < roles.length; i++) {
+    if (roles[i].name === "moderator") {
+      next();
+      return;
+    }
+  }
+  return res.status(403).json({ message: "Required moderator role" });
+};
+
+export const isAdmin = async (req, res, next) => {
+  const user = await User.findById(req.userId);
+  const roles = await Role.find({ _id: { $in: user.roles } });
+  for (let i = 0; i < roles.length; i++) {
+    if (roles[i].name === "admin") {
+      next();
+      return;
+    }
+  }
+  return res.status(403).json({ message: "Required admin role" });
 };
