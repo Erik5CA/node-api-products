a306600c6c1cbefcf5bc2a8c06aece5fa130cd46
Erik5CA <erik.castillo.anzures@gmail.com>
2024-12-13 15:36:38 -0600

    add new route for users

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import morgan from "morgan";
 import productsRoutes from "./routes/product.routes.js";
 import authRoutes from "./routes/auth.routes.js";
+import userRoutes from "./routes/user.routes.js";
 import { createRoles } from "./libs/initalSetup.js";
 
 const app = express();
@@ -20,5 +21,6 @@ app.get("/", (req, res) => {
 
 app.use("/api/products", productsRoutes);
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 
 export default app;
