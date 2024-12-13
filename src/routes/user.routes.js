831848ff4a20dbf9acd291515b67f4d4624548a1
Erik5CA <erik.castillo.anzures@gmail.com>
2024-12-13 15:41:11 -0600

    add new route user with middlewares

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,1 +1,9 @@
 import { Router } from "express";
+import { createUser } from "../controllers/user.controller.js";
+import { isAdmin, verifyToken } from "../middleware/authJwt.js";
+import { checkRolesExisted } from "../middleware/verifySignup.js";
+const router = Router();
+
+router.post("/", [verifyToken, isAdmin, checkRolesExisted], createUser);
+
+export default router;
