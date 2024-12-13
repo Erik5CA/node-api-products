990a83cc3faffeb2af6ee89aa85738c104d1020f
Erik5CA <erik.castillo.anzures@gmail.com>
2024-12-13 15:39:50 -0600

    add middlewares to signup route

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,10 +1,14 @@
 import { Router } from "express";
 import { signIn, signUp } from "../controllers/auth.controller.js";
+import {
+  checkRolesExisted,
+  checkUserExist,
+} from "../middleware/verifySignup.js";
 
 const router = Router();
 
 router.post("/signin", signIn);
 
-router.post("/signup", signUp);
+router.post("/signup", [checkUserExist, checkRolesExisted], signUp);
 
 export default router;
